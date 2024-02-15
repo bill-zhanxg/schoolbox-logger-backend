aa3e8f470fb871ae305a3aa0b034c75d274a5560
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-02-15 20:08:33 +1100

    fix getting portrait unauthed, use cookie instead

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,115 @@
 import 'dotenv-flow/config';
 
-import express, { NextFunction } from 'express';
+const dynamicImport = new Function('specifier', 'return import(specifier)');
+
+import * as cheerio from 'cheerio';
+import express, { NextFunction, Request, Response } from 'express';
+import type QueueType from 'queue';
 import { getXataClient } from './libs/xata';
 
-console.log(process.env.XATA_API_KEY);
 const app = express();
-const client = getXataClient();
+const xata = getXataClient();
+app.use(express.json());
 
 export function authenticatedUser(req: Request, res: Response, next: NextFunction) {
-	console.log('authenticatedUser');
-	// if (!session?.user) {
-	// 	res.redirect('/login');
-	// } else {
-	// 	next();
-	// }
+	const secret = req.headers.authorization;
+	if (!secret || secret !== process.env.AUTH_SECRET) res.status(401).send('Unauthorized');
+	else next();
 }
 
 app.use(express.json());
+
+app.post('/', authenticatedUser, async (req, res) => {
+	const { schoolboxDomain, schoolboxCookie } = req.body;
+	// Validate request body
+	if (!schoolboxDomain || !schoolboxCookie) return res.status(400).send('Incomplete request body');
+	let schoolboxUrl: string;
+	try {
+		schoolboxUrl = new URL(schoolboxDomain).href;
+	} catch (err) {
+		return res.status(400).send('Invalid schoolbox domain');
+	}
+
+	const Queue = await dynamicImport('queue');
+	const q: QueueType = new Queue.default();
+
+	for (let i = 8294; i < 8295; i++) {
+		q.push((cb) => {
+			if (!cb) throw new Error('Callback is not defined');
+
+			fetch(`${schoolboxUrl}search/user/${i}`, {
+				// Originally I used schoolbox token but portrait API require cookie so why not both use cookie instead?
+				headers: {
+					cookie: schoolboxCookie,
+				},
+			})
+				.then(async (res) => {
+					if (res.ok) {
+						const $ = cheerio.load(await res.text());
+						const email = $('#content .content dd a').text().toLowerCase();
+						if (email.trim()) {
+							console.log(email);
+							// We need user cookie to get portrait
+							fetch(`${schoolboxUrl}portrait.php?id=${i}`, {
+								headers: {
+									cookie: schoolboxCookie,
+								},
+							})
+								.then(async (res) => {
+									console.log(res.status);
+									if (res.ok) {
+										const contentDisposition = res.headers.get('content-disposition');
+										if (!contentDisposition) throw new Error('Content-Disposition header is not defined');
+										const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+										const matches = filenameRegex.exec(contentDisposition);
+										if (matches != null && matches[1]) {
+											const filename = matches[1].replace(/['"]/g, '');
+											console.log(filename);
+											const portraitBlob = await res.blob();
+											new File([portraitBlob], filename);
+
+											// Database action
+											// xata.db.portraits
+											// 	.create({
+											// 		mail: email,
+											// 		portrait: portrait,
+											// 	})
+											// 	.catch((err) => {
+											// 		// TODO
+											// 		console.log('database error', err);
+											// 	})
+											// 	.finally(() => {
+											// 		cb();
+											// 	});
+										} else throw new Error('Filename is not defined');
+									} else throw new Error(`Failed getting portrait for ${i} with status ${res.status}`);
+								})
+								.catch((err) => {
+									// TODO
+									console.log(err);
+									cb();
+								});
+						}
+					} else {
+						console.log(res.statusText);
+						cb();
+					}
+				})
+				.catch((err) => {
+					// TODO
+					console.log(err);
+					cb();
+				});
+		});
+	}
+
+	q.start((err) => {
+		// TODO
+		if (err) throw err;
+		console.log('all done:', q);
+	});
+	res.send('authed');
+});
 
 app.listen(8000, () => {
 	console.log('listening on 8000');
---
+++ src/libs/getXataFile.ts
@@ -0,0 +1,34 @@
+export async function getXataFile(
+	file?: File | null,
+	enablePublicUrl = true,
+): Promise<
+	| [
+			{
+				name: string;
+				mediaType: string;
+				base64Content: string;
+				enablePublicUrl: boolean;
+			},
+			null,
+	  ]
+	| [null, File]
+	| [null, null]
+	| string
+> {
+	if (file && file.size > 104_000_000) return 'Evidence file size can not be larger than 104MB';
+
+	if (!file || (file.name === 'undefined' && file.size === 0 && file.type === 'application/octet-stream'))
+		return [null, null];
+	if (file.size < 20_000_000) {
+		const arrayBuffer = await file.arrayBuffer();
+		return [
+			{
+				name: file.name,
+				mediaType: file.type,
+				base64Content: Buffer.from(arrayBuffer).toString('base64'),
+				enablePublicUrl,
+			},
+			null,
+		];
+	} else return [null, file];
+}
